8e678d0b32c9fa9caa7680b48a9d1277f7fdd053
Frank  D'Angelo <08fdange@gmail.com>
2020-02-29 20:28:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  @@words
+  attr_accessor :word
+  
+  def words
+    @@words
+  end
+  
+  def initialize(word)
+    @word = word
+    @@words << word
+  end
+  
+  def match
+    @@words.find? do |word|
+      self
+  
+end
